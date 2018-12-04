1e7959326d8ed57ff4a97266346a17c07fb0c42b
wesley184 <wesley184@163.com>
2018-12-04 11:28:11 +0800

    Site updated: 2018-12-04 11:28:03

--- js/app.js
+++ js/app.js
@@ -120,12 +120,6 @@ var customSearch;
 		})
 	}
 	function setWaves() {
-		Waves.attach('.flat-btn', ['waves-button']);
-		Waves.attach('.float-btn', ['waves-button', 'waves-float']);
-		Waves.attach('.float-btn-light', ['waves-button', 'waves-float', 'waves-light']);
-		Waves.attach('.flat-box', ['waves-block']);
-		Waves.attach('.float-box', ['waves-block', 'waves-float']);
-		Waves.attach('.waves-image');
 		Waves.init();
 	}
 	function setScrollReveal() {
